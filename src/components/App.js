bb67c60e138e154086c7ba432fb2ce6f8f3235c1
lusan <daslusan@gmail.com>
2018-04-29 18:45:49 +0530

    init commit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '../containers/Header'
+import MainSection from '../containers/MainSection'
+
+const App = () => (
+  <div className='main'>
+    {/* <Header />
+    <MainSection /> */}
+    Hello
+  </div>
+)
+
+export default App
